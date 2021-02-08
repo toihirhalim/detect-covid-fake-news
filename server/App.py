c0051c0745b35868f20690adb1a27663b968ae6f
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-02-08 18:13:47 +0100

    adding tokenization by words and by phrases

--- server/App.py
+++ server/App.py
@@ -1,5 +1,7 @@
 import strawberry
 from Scrapping import get_text
+from Tokenize import tokenize_by_words, tokenize_by_sentences
+from typing import List
 
 
 @strawberry.type
@@ -7,6 +9,13 @@ class Query:
     @strawberry.field
     def scrapping(self, url: str) -> str:
         return get_text(url)
+
+    @strawberry.field
+    def tokenize(self, text: str, words: bool = True ) -> List[str]:
+        if words:
+            return tokenize_by_words(text)
+
+        return tokenize_by_sentences(text)
 
 
 schema = strawberry.Schema(query=Query)
